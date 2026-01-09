c477a0590b76827ad329585bcacdcf502bb0ed27
aqshelhazarya <axelhazarya25@gmail.com>
2026-01-10 03:23:00 +0700

    feat: add assets images and finalize UI for UAS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb"
+      }
+    }
+  },
+  plugins: [],
+}
